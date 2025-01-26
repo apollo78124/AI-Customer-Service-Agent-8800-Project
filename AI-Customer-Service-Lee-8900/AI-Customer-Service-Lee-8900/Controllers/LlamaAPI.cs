d5efd080cf499222d14e442d2e5e084e47a43e1b
apollo78124 <98apollo@naver.com>
2025-01-26 11:03:26 -0800

    APi controller

---
+++ AI-Customer-Service-Lee-8900/AI-Customer-Service-Lee-8900/Controllers/LlamaAPI.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace AI_Customer_Service_Lee_8900.Controllers
+{
+    [Route("api")]
+    [ApiController]
+    public class LlamaAPI : ControllerBase
+    {
+        // GET: api/<LlamaAPI>
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/<LlamaAPI>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<LlamaAPI>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<LlamaAPI>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<LlamaAPI>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}

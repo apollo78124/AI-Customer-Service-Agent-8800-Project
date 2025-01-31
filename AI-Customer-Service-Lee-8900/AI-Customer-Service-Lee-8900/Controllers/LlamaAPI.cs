be1df1393438473176a199ff05a2f0d4b48ebacb
apollo78124 <98apollo@naver.com>
2025-01-30 20:41:40 -0800

    Footer added

--- AI-Customer-Service-Lee-8900/AI-Customer-Service-Lee-8900/Controllers/LlamaAPI.cs
+++ AI-Customer-Service-Lee-8900/AI-Customer-Service-Lee-8900/Controllers/LlamaAPI.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.AI;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -8,11 +9,52 @@ namespace AI_Customer_Service_Lee_8900.Controllers
     [ApiController]
     public class LlamaAPI : ControllerBase
     {
+        IChatClient chatClient;
+        List<ChatMessage> chatHistory = new();
+        public LlamaAPI ()
+        {
+            chatClient =
+    new OllamaChatClient(new Uri("http://localhost:11434/"), "llama3.1");
+
+        }
+
+        //[HttpGet]
+        //public string GetAsync()
+        //{
+        //    var userPrompt = "Hi, What's your name?";
+        //    //chatHistory.Add(new ChatMessage(ChatRole.User, userPrompt));
+
+        //    var response = "string1";
+
+        //    //    var task = chatClient.CompleteStreamingAsync(chatHistory);
+        //    //    await foreach (var item in
+        //    //    chatClient.CompleteStreamingAsync(chatHistory))
+        //    //    {
+        //    //        response += item.Text;
+        //    //    }
+        //    //    chatHistory.Add(new ChatMessage(ChatRole.Assistant, response));
+
+        //    return response;
+        //}
+
         // GET: api/<LlamaAPI>
         [HttpGet]
-        public IEnumerable<string> Get()
+        public async Task<IEnumerable<string>> GetAsync()
         {
-            return new string[] { "value1", "value2" };
+            var userPrompt = "Hi, What's your name?";
+            chatHistory.Add(new ChatMessage(ChatRole.User, userPrompt));
+
+            var response = "";
+
+            var task = chatClient.CompleteStreamingAsync(chatHistory);
+            await foreach (var item in
+            chatClient.CompleteStreamingAsync(chatHistory))
+            {
+                response += item.Text;
+            }
+            chatHistory.Add(new ChatMessage(ChatRole.Assistant, response));
+
+            return new string[] { response };
         }
 
         // GET api/<LlamaAPI>/5
@@ -22,10 +64,23 @@ namespace AI_Customer_Service_Lee_8900.Controllers
             return "value";
         }
 
-        // POST api/<LlamaAPI>
         [HttpPost]
-        public void Post([FromBody] string value)
+        public async Task<IEnumerable<string>> PostAsync([FromBody] string value)
         {
+            var userPrompt = "Hi, What's your name?";
+            chatHistory.Add(new ChatMessage(ChatRole.User, userPrompt));
+
+            var response = "";
+
+            var task = chatClient.CompleteStreamingAsync(chatHistory);
+            await foreach (var item in
+            chatClient.CompleteStreamingAsync(chatHistory))
+            {
+                response += item.Text;
+            }
+            chatHistory.Add(new ChatMessage(ChatRole.Assistant, response));
+
+            return new string[] { response };
         }
 
         // PUT api/<LlamaAPI>/5
